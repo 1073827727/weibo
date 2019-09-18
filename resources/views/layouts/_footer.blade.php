e4e68bb80d22828241e5fd22aab95d2c36859a3a
guo <1073827727@qq.com>
2019-09-18 09:36:21 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+
+    <a href="">______________________________________________________________</a>
+    <div class="float-right">
+        <a href="/about">关于</a>
+    </div>
+</footer>
