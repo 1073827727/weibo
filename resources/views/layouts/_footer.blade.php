71a19b1d60ea8b780df9fda6439473cd1c99dbc7
guo <1073827727@qq.com>
2019-09-18 09:50:31 +0000

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,6 +2,6 @@
 
     <a href="">______________________________________________________________</a>
     <div class="float-right">
-        <a href="/about">关于</a>
+        <a href="{{ route('about') }}">关于</a>
     </div>
 </footer>
