fe03479d9f7ace99346ba742f9a6ad878080aa4d
guo <1073827727@qq.com>
2019-09-18 06:44:48 +0000

    base view

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+  <title>@yield('title','weibo app')</title>
+</head>
+<body>
+  <h1>@yield('content')</h1>
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title','weibo app')
+
+@section('content')
+  <h1>主页</h1>
+@stop
