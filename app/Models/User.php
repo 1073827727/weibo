6d87d7d06f70beeb14ec6761b74bb31d62192c8a
guo <1073827727@qq.com>
2019-09-26 09:56:48 +0000

    zhanshi

--- app/Models/User.php
+++ app/Models/User.php
@@ -68,5 +68,13 @@ class User extends Authenticatable
     }
 
 
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
 
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
 }
